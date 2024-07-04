730153b6f5fcee285638da92f633d82e6032cf55
Rafaela <teixeiranunesrafaela@gmail.com>
2024-07-03 23:55:04 -0700

    Text menu

--- code/Player.py
+++ code/Player.py
@@ -1,1 +1,11 @@
+import pygame.key
 
+from code.Entity import Entity
+
+
+class Player(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+    def move(self):
+        pass
